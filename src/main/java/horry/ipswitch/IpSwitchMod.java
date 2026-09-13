f4fef0f18d707f50c5fb0a4834552a507df24fdc
qweg0 <aleksandrlobenko27@gmail.com>
2026-09-14 00:00:35 +0700

    Update IpSwitchMod.java

--- src/main/java/horry/ipswitch/IpSwitchMod.java
+++ src/main/java/horry/ipswitch/IpSwitchMod.java
@@ -5,10 +5,16 @@ import mindustry.mod.Mod;
 
 public class IpSwitchMod extends Mod {
 
+    static {
+        Log.info("[IP Switch] STATIC BLOCK RUNS");
+    }
+
+    public IpSwitchMod() {
+        Log.info("[IP Switch] CONSTRUCTOR RUNS");
+    }
+
     @Override
     public void init() {
-        Log.info("[IP Switch] ================");
-        Log.info("[IP Switch] INIT CALLED OK");
-        Log.info("[IP Switch] ================");
+        Log.info("[IP Switch] INIT RUNS");
     }
 }
